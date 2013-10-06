84625c78002570075d8abc4409429c0b8575fae2
Abigail Jones <AbbyJonesDev@gmail.com>
2013-10-06 09:53:39 -0500

    In progress on Create_Articles feature...

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -11,4 +11,6 @@ When(/^I fill in "(.*?)" with "(.*?)"$/) do |field, value|
   fill_in(field, :with => value)
 end
 
-
+And /I attach a photo with path "(.*)"/ do |path|
+  page.attach_file(:photo, path)
+end
